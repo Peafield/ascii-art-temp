2c4ec7cc88fe2c88f04b955306f678d9ccf97b37
peafield <peterjohncoles@gmail.com>
2022-10-13 19:28:35 +0100

    Working version except '\n'

--- printascii.go
+++ printascii.go
@@ -3,14 +3,27 @@ package asciiart
 import "fmt"
 
 func AcciiCalc(r int) int {
-	return ((r - 27) * 8) - 2
+	return ((r - 32) * 9) + 1
 }
 
 func PrintAscii(s []string, a string) {
 	str := []rune(a)
-	for _, v := range str {
-		for i := 0; i < 8; i++ {
-			fmt.Println(s[i+AcciiCalc(int(v))])
+	rows := make([]string, 8)
+	for v := 0; v < len(str); v++ {
+		if str[v] == 10 {
+			PrintRows(rows)
+			rows = make([]string, 8)
+		} else {
+			for i := 0; i < 8; i++ {
+				rows[i] += (s[i+AcciiCalc(int(str[v]))])
+			}
 		}
+	}
+	PrintRows(rows)
+}
+
+func PrintRows(rows []string) {
+	for i := 0; i < 8; i++ {
+		fmt.Println(rows[i])
 	}
 }
