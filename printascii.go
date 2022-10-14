8bd8ae4b500ea65c58517a6e0b5d20c198fa1ab0
peafield <peterjohncoles@gmail.com>
2022-10-14 09:28:38 +0100

    Return case solution

--- printascii.go
+++ printascii.go
@@ -6,8 +6,18 @@ func AcciiCalc(r int) int {
 	return ((r - 32) * 9) + 1
 }
 
+func changeReturn(r []rune) []rune {
+	for i, v := range r {
+		if v == '\\' && r[i+1] == 'n' {
+			r = append(r[:i], r[i+1:]...)
+			r[i] = 10
+		}
+	}
+	return r
+}
+
 func PrintAscii(s []string, a string) {
-	str := []rune(a)
+	str := changeReturn([]rune(a))
 	rows := make([]string, 8)
 	for v := 0; v < len(str); v++ {
 		if str[v] == 10 {
