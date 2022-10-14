e3dbc0bd8623a81e635f075c091b1dcd57a0f9d6
Peter J Coles <peterjohncoles@gmail.com>
2022-10-14 11:30:08 +0100

    Version which passes all tests

--- printascii.go
+++ printascii.go
@@ -6,34 +6,41 @@ func AcciiCalc(r int) int {
 	return ((r - 32) * 9) + 1
 }
 
-func changeReturn(r []rune) []rune {
-	for i, v := range r {
-		if v == '\\' && r[i+1] == 'n' {
-			r = append(r[:i], r[i+1:]...)
-			r[i] = 10
-		}
-	}
-	return r
-}
-
 func PrintAscii(s []string, a string) {
-	str := changeReturn([]rune(a))
+	str := []rune(a)
 	rows := make([]string, 8)
 	for v := 0; v < len(str); v++ {
-		if str[v] == 10 {
-			PrintRows(rows)
+		if str[v] == 92 && str[v+1] == 110 {
+			if !IsStringArrEmpty(rows) {
+				PrintRows(rows)
+			} else {
+				fmt.Print("\n")
+			}
 			rows = make([]string, 8)
+			v += 1
 		} else {
 			for i := 0; i < 8; i++ {
 				rows[i] += (s[i+AcciiCalc(int(str[v]))])
 			}
 		}
 	}
-	PrintRows(rows)
+	if !IsStringArrEmpty(rows) {
+		PrintRows(rows)
+	}
 }
 
 func PrintRows(rows []string) {
 	for i := 0; i < 8; i++ {
 		fmt.Println(rows[i])
 	}
+}
+
+func IsStringArrEmpty(rows []string) bool {
+	empty := make([]string, 8)
+	for i := 0; i < len(rows); i++ {
+		if rows[i] != empty[i] {
+			return false
+		}
+	}
+	return true
 }
